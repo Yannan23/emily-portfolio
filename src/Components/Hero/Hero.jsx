141294e2c41b18fc36ca155cbee682c91fa496eb
Yannan23 <yannansun0723@gmail.com>
2025-01-18 16:09:48 +1100

    changed name

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div id='home' className='hero'>
       <div className='hero-img'></div>
       {/* <img src={profile_img} alt="" /> */}
-      <h1><span>I‘m Yannan Sun,</span> full stack developer based in Sydney.</h1>
+      <h1><span>I &apos; m Emily Sun,</span> full stack developer based in Sydney.</h1>
       <p>I am a dedicated Junior Full Stack Developer with strong communication and teaching skills. I excel at problem-solving and have a passion for learning new technologies.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
