948eec45e45d621c731c308e6939702c10b78f2e
Yannan23 <yannansun0723@gmail.com>
2025-01-18 16:04:15 +1100

    updated emily portfolio

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,18 @@
+import project_1_img from './project_1.svg'
+import project_2_img from './project_2.svg'
+
+const mywork_data = [
+  {
+    w_no: 1,
+    w_name: 'Learn Wise',
+    w_img: project_1_img,
+    w_link: 'https://www.learn-wise.live/'
+  }
+  // {
+  //   w_no: 2,
+  //   w_name: 'Pokemon world',
+  //   w_img: project_2_img,
+  //   w_link: 'https://www.learn-wise.live/'
+  // }
+]
+export default mywork_data
